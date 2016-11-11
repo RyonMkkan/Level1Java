fbdf948aa28ea1bebe8e01256acc953bdace0fce
Ryon <league@Mac-mini.local>
2016-11-10 16:57:37 -0800

    STUDDY

---
+++ src/spongebob.java
@@ -0,0 +1,10 @@
+
+public class spongebob {
+public static void main(String[] args) {
+	SeaCreature plat= new SeaCreature("Spongebob");
+	SeaCreature pat= new SeaCreature("patrick");
+	SeaCreature squid=new SeaCreature
+	plat.eat();
+	
+}
+}
