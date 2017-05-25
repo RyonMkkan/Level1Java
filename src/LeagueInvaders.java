88477f00610a21eea7f77ca6344cd526b56f5db3
CeilaScarlet <league@Admins-Mac-mini.local>
2017-05-25 16:57:26 -0700

    lo I'm done
    ha lol

---
+++ src/LeagueInvaders.java
@@ -0,0 +1,27 @@
+import javax.swing.JFrame;
+
+public class LeagueInvaders {
+	int width=500;
+	int height=800;
+	
+	JFrame frame = new JFrame();
+	
+public static void main(String[] args) {
+	
+		
+}
+LeagueInvaders(){
+	
+	
+	
+	
+}
+ void setup(){
+	 frame.setVisible(true);
+	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	 
+ }
+ }
+
+
+
