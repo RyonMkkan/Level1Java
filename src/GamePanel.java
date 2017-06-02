aced14407799e4324fe8c927639ec5a1171d8d49
joshpryor <league@Admins-Mac-mini-2.local>
2017-06-01 17:04:06 -0700

    leave invaders
    part 2

---
+++ src/GamePanel.java
@@ -0,0 +1,31 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+public class GamePanel extends JPanel implements ActionListener{
+Timer time;
+
+
+GamePanel(){
+	
+	
+	
+}
+
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	System.out.println("nugget");
+}
+
+	
+	
+	
+	
+	
+
+}
+
+
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -3,23 +3,22 @@ import javax.swing.JFrame;
 public class LeagueInvaders {
 	int width=500;
 	int height=800;
-	
 	JFrame frame = new JFrame();
 	
 public static void main(String[] args) {
-	
+	LeagueInvaders window = new LeagueInvaders();
 		
 }
+
 LeagueInvaders(){
 	
-	
-	
-	
+	setup();	
 }
+
  void setup(){
 	 frame.setVisible(true);
 	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-	 
+	 frame.setSize(width, height);
  }
  }
 
