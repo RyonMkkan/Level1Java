ae68e42c9d536f6f1014ea622aba3167608c7465
League <Level 2>
2017-06-08 16:55:45 -0700

    gAME PANEL WORK
    stuff about game panel and oops caps was on

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,26 +1,34 @@
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
-public class GamePanel extends JPanel implements ActionListener{
-int time;
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
+Timer time;
 
 
 GamePanel(){
-	time  = 1000/60;
-	ActionListener.this
-	
-	
-	
-	
+	System.out.println("I");
+	time = new Timer(1000/60, this);
 }
+void startGame(){
+	System.out.println("lIKE");
+	time.start();
+}
+public void paintComponent(Graphics g){
+	g.fillRect(10, 10, 100, 100);
+	System.out.println("TO");
+}
+
 
 
 @Override
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
-	System.out.println("nugget");
+	repaint();
+	System.out.println("EAT");
 }
 
 	
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,26 +1,36 @@
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 public class LeagueInvaders {
 	int width=500;
 	int height=800;
 	JFrame frame = new JFrame();
-	
+	GamePanel GamePaneel;
 public static void main(String[] args) {
 	LeagueInvaders window = new LeagueInvaders();
+		
 		
 }
 
 LeagueInvaders(){
-	
+	GamePaneel = new GamePanel();
 	setup();	
+	
+	
 }
-
  void setup(){
+	frame.add(GamePaneel);
+	frame.addKeyListener(GamePaneel);
 	 frame.setVisible(true);
 	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 	 frame.setSize(width, height);
+	 GamePaneel.startGame();
+	 
  }
- }
+}
+ 
 
 
 
